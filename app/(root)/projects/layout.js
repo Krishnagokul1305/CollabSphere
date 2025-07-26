aded20a9560f1d1c8b7e3725b38a273742d08ac0
Krishnagokul1305 <krishnagokul1729@gmail.com>
2025-07-26 18:47:05 +0530

    added metadata for all pages

---
+++ app/(root)/projects/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "CollabSphere | Projects",
+  description: "Manage your projects and Add collaborators and more.",
+};
+
+function layout({ children }) {
+  return children;
+}
+
+export default layout;
---
+++ app/(root)/todo/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "CollabSphere | Todos",
+  description: "Manage your Todos",
+};
+
+function layout({ children }) {
+  return children;
+}
+
+export default layout;
